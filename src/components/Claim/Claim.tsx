335a29242a5f0e5d7957ac1458882922b3468aa7
folathecoder <folathecoder@gmail.com>
2022-04-05 20:01:09 +0100

    Implemented the claim UI with all states

---
+++ src/components/Claim/Claim.tsx
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import './ClaimStyles.css';
+import { contractABI, contractAddress } from '../utils/constants';
+import { ethers } from 'ethers';
+
+const Claim = () => {
+  //set to true if wallet is connected
+  const [isWalletConnected, setIsWalletConnected] = useState(false);
+
+  //set to true if the balance is claimed
+  const [claimed, setClaimed] = useState(false);
+
+  // set to true when the claim() function is called and yet to be fulfilled
+  const [claimLoading, setClaimLoading] = useState(false);
+
+  // set to true of balance is Zero
+  const [noClaim, setNoClaim] = useState(true);
+  const [currentAccount, setCurrentAccount] = useState<string>('');
+
+  return (
+    <main>
+      <section className='claim__container'>
+        {!isWalletConnected ? (
+          <div className='claim_message-container'>
+            <div>
+              <div className='claim_message'>
+                <div>
+                  <p>
+                    Connect your wallet in order to claim your $PING rewards
+                  </p>
+                </div>
+                <div className='connect_button'>
+                  <button>Connect your wallet</button>
+                </div>
+                <div>
+                  <p className='error_message'>
+                    Note: Install metamask to connect
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div className='claim_card-container'>
+            <div>
+              <div className='claim_card'>
+                <div>
+                  <p>{claimed ? 'You claimed' : 'Available to claim'}</p>
+                </div>
+                <div>
+                  <p className='claim_balance'>1,000,000.00</p>
+                  <p className='claim_token'>$PING</p>
+                </div>
+                <div>
+                  <p className={'address'}>{`${currentAccount.slice(
+                    0,
+                    8
+                  )}...${currentAccount.slice(
+                    currentAccount.length - 4,
+                    currentAccount.length
+                  )}`}</p>
+                </div>
+                <div>
+                  <>
+                    {!claimLoading && !claimed && !noClaim && (
+                      <button>Claim</button>
+                    )}
+                  </>
+                  <>{claimLoading && <button>Claiming...</button>}</>
+                  <>{claimed && <button>Transactions</button>}</>
+                  <>{noClaim && <button>Not Correct?</button>}</>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </section>
+    </main>
+  );
+};
+
+export default Claim;
---
+++ src/components/utils/constants.ts
@@ -0,0 +1,4 @@
+import abi from './abi.json';
+
+export const contractAddress = '0x45aaF97b43971A499f68be18A20654939c71607E';
+export const contractABI = abi;
